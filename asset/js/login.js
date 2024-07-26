6b4f2cb50c8b49fee5dcae808486c10e9c327df3
Talented123 <okonkwoarinze16@gmail.com>
2024-07-26 10:57:17 +0100

    second commit

--- asset/js/login.js
+++ asset/js/login.js
@@ -3,14 +3,16 @@
 
 const form = document.getElementById("login");
 
-const validate = localStorage.getItem('myForms');
+const validate = localStorage.getItem ('myForms');
 
 form.addEventListener("submit", (e) => {
+    e.preventDefault()
     if(validate) {
         const inputFields = JSON.parse(validate)
         if(form.email.value == inputFields.email && form.password.value == inputFields.password) {
 
             alert("welcome login successful.");
+            location.href = "../../landing.html"
             const newText = {
                 email: inputFields.email,
                 password: inputFields.password
@@ -18,7 +20,6 @@ form.addEventListener("submit", (e) => {
 
             sessionStorage.setItem("logins", JSON.stringify(newText))
 
-            location.href = "landing.html"
         }  else {
             alert("you are wrong,try again")
         }
